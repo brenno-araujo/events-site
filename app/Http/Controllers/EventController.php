44dcb92b52e13540a048ebbe5e5fc3ad01bbdceb
B4OS <brennoaraujo@yahoo.com>
2021-05-03 10:09:54 -0300

    Migration e Model

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -13,7 +14,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        $events = Event::all();
+
+        return view('welcome',['events'=>$events]);
     }
 
     /**
