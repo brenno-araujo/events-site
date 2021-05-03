868cba0dad03381a695f0cb9381623ceba2030dc
B4OS <brennoaraujo@yahoo.com>
2021-05-03 10:33:50 -0300

    Store

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -37,7 +37,16 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect('/');
     }
 
     /**
