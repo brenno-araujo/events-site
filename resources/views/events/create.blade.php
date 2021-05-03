e1387a34d53f31192c0304c90cb6879930382346
B4OS <brennoaraujo@yahoo.com>
2021-05-03 11:08:16 -0300

    Store IMG

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -8,12 +8,12 @@
   <h1>Crie o seu evento</h1>
   <form action="/events" method="POST" enctype="multipart/form-data">
     @csrf
-    {{--
+
     <div class="form-group">
       <label for="image">Imagem do Evento:</label>
       <input type="file" id="image" name="image" class="from-control-file">
     </div>
-    --}}
+
     <div class="form-group">
       <label for="title">Evento:</label>
       <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
